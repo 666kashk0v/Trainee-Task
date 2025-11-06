f824ebc44ea63317bab2da884066103b2f5d20b1
Ivan Kashkov <kashkov06@gmail.com>
2025-11-06 18:12:20 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/AddName.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraineeshipTask
+{
+    class AddName
+    {
+        //Input fields
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        //Additional field
+        public string emailAddress { get; set; } = null;
+        //Constructor for init "Input fields"
+        public AddName(string firstName, string lastName, string middleName) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleName = middleName;
+        }
+        //Empty constructor if we want to init classes field manually
+        public AddName() { }
+        //Callback method which delete spaces and sets the correct cases
+        private void FormatName()
+        {
+            FirstName = FirstName.Replace(" ", "");
+            LastName = LastName.Replace(" ", "");
+            MiddleName = MiddleName.Replace(" ", "");
+
+            FirstName = char.ToUpper(FirstName[0]) + FirstName.Substring(1).ToLower();
+            LastName = char.ToUpper(LastName[0]) + LastName.Substring(1).ToLower();
+            MiddleName = char.ToUpper(MiddleName[0]) + ".";
+        }
+        //Callback method which check status of "emailAddress" field
+        private bool IsEmail() 
+        {
+            if (emailAddress == null || emailAddress.Contains("@") == false) //check the "@" sign
+            {
+                return false;
+            }
+            else 
+            {
+                return true;
+            }
+        }
+        //Method which return formated name
+        public string ShowFormatedName()
+        {
+            string result;
+            FormatName();
+            if (IsEmail()) 
+            {
+                Console.WriteLine("Email adress was detected");
+                result = $"{FirstName} {LastName} {MiddleName} {emailAddress}";
+            }
+            else 
+            {
+                Console.WriteLine("Error. Field \"Email address\" is empty or incorrect");
+                result = $"{FirstName} + {LastName} + {MiddleName}";
+            }
+            return result;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraineeshipTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Use constructor and add manually incorrect "emailAdress" field
+            AddName name = new AddName("Иванов", "Иван", "Иванович");
+            name.emailAddress = "ivanoviimail.ru";
+            Console.WriteLine(name.ShowFormatedName());
+            Console.ReadLine();
+
+            //Set input fields manually, also set additional field("emailAdress").
+            AddName name1 = new AddName();
+            name1.FirstName = "Дмит рий";
+            name1.LastName = "Дми триен ко";
+            name1.MiddleName = "Дмит риев";
+            name1.emailAddress = "dmitrydd@gmail.com";
+            Console.WriteLine(name1.ShowFormatedName());
+            Console.ReadLine();
+        }
+    }
+}
